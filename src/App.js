78b31bf988ac4c5ed491a545c461c5a43df2dec7
Bhavin305 <bhavin.hariyani@bidchat.com>
2019-07-05 17:59:26 +0530

    Update code

--- src/App.js
+++ src/App.js
@@ -6,23 +6,23 @@ import Starwars from './containers/Starwars';
 import history from './services/history';
 import './index.css';
 
-const Routers = (props) =>{
-  console.log('props', props);
-  return(
-    <Switch>
-      <Route path="/" exact component={Login}/>
-      <Sample path="/starwars" exact component={Starwars}/>
-      <Route component={NoMatch} />
-    </Switch>
-  );
-}
 
-const Sample = (props) => {
+const Authenticate = (props) => {
   if (localStorage.getItem('session')) {
     return <Route path={props.path} exact={props.exact} render={() => <props.component />} />;
   }
   return <Route path={props.path} exact={props.exact} render={() => <Redirect to="/" />} />;
 };
+
+const Routers = (props) =>{
+  return(
+    <Switch>
+      <Route path="/" exact component={Login}/>
+      <Authenticate path="/starwars" exact component={Starwars}/>
+      <Route component={NoMatch} />
+    </Switch>
+  );
+}
 
 const App = () => (
   <Fragment>
